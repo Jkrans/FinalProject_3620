1c0b6abcd4ed7de83c76b37bed8c0d7898e1ea24
Jeremy Krans <49561917+Jkrans@users.noreply.github.com>
2023-10-23 16:46:43 -0600

    deploy instructions for Railway

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.views.generic import RedirectView
 from django.urls import path, include
 from Users import views as user_views
 from django.contrib.auth import views as auth_views
@@ -22,6 +23,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='login')),
     path('', include('mainapp.urls')),
     path('register', user_views.register, name='Register'),
     path('login', auth_views.LoginView.as_view(
