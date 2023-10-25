70ca3266a47ccdfdd18497ac96b2b43c4f2e57ce
Jeremy Krans <49561917+Jkrans@users.noreply.github.com>
2023-10-25 11:22:17 -0600

    updated settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://santasbag.up.railway.app/']
+ALLOWED_HOSTS = ['santasbag.up.railway.app']
 
 CSRF_COOKIE_SECURE = True
 
